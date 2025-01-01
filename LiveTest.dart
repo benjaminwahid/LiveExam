d7465ed48f4b084aaa8daf1b3bf31f384de97023
Benjamin Wahid Pico <bwpico@gmail.com>
2025-01-01 23:33:20 +0600

    Initial commit

--- LiveTest.dart
+++ LiveTest.dart
@@ -1,4 +1,22 @@
-//test github 2
+//test github
+import 'dart:io';
 void main(){
-  print ("Live Test: ");
+  triangle();
+}
+void triangle(){
+  stdout.write("Enter the triangle base: ");
+  double? base = double.tryParse(stdin.readLineSync()!);
+
+
+  stdout.write("Enter the triangle height: ");
+  double? height = double.tryParse(stdin.readLineSync()!);
+
+  if(base != null && height != null){
+    double? TriangleArea=0.5*base*height;
+    print("The triangle Area is: $TriangleArea");
+  }
+  else{
+    print("Please enter the correct input of base and height. Thank you.");
+  }
+
 }
